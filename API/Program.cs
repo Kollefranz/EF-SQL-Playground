e9eb7c44c1ff240420dfd742c7ffe6a656619a87
Your Name <you@example.com>
2026-03-27 18:18:21 +0100

    fix: Swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,15 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
+
+app.MapOpenApi();
+app.MapScalarApiReference(string.Empty);
 
 app.UseAuthorization();
 
